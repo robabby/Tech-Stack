c483a72b35a18744f499099253d8d26f214c0e90
robabby <robabby23@gmail.com>
2017-05-23 08:22:08 -0500

    Wired action creator to component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles
-
+    console.log(this.props);
     return (
       <CardSection>
         <Text style={titleStyle}>{this.props.library.title}</Text>
@@ -21,4 +23,4 @@ const styles = {
   }
 }
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
