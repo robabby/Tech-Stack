7f6bdaf478417da91c2415eeedf035425625bfa6
robabby <robabby23@gmail.com>
2017-05-24 07:33:01 -0500

    Implemented touchable element

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
-    const { titleStyle } = styles
-    console.log(this.props);
+    const { titleStyle } = styles;
+    const { id, title } = this.props.library;
+
     return (
-      <CardSection>
-        <Text style={titleStyle}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
