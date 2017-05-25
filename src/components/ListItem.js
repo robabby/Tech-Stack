710c633999d6a55d2932452c810ea8ed5581089a
robabby <robabby23@gmail.com>
2017-05-25 07:25:08 -0500

    Uodated the mapStateToProps method

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,10 +5,20 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
-
+    console.log(this.props)
     return (
       <TouchableWithoutFeedback
         onPress={() => this.props.selectLibrary(id)}
@@ -19,6 +29,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -32,4 +43,12 @@ const styles = {
   }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
